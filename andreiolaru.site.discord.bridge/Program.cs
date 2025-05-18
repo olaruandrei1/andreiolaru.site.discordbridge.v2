09b413c893babcdc778b2eef59d22e72346fadc4
andreiolaru <development@andreiolaru.com>
2025-05-18 16:51:09 +0300

    Auto migrate

--- andreiolaru.site.discord.bridge/Program.cs
+++ andreiolaru.site.discord.bridge/Program.cs
@@ -9,6 +9,7 @@ using andreiolaru.site.discord.Dispatchers;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,29 @@ builder.Services.AddDispatchers();
 builder.Services.AddDomain(builder.Configuration);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    var retryCount = 0;
+    var connected = false;
+    while (!connected && retryCount < 10)
+    {
+        try
+        {
+            db.Database.Migrate();
+            connected = true;
+        }
+        catch
+        {
+            retryCount++;
+            Console.WriteLine($"Database not ready yet... retrying ({retryCount}/10)");
+            Thread.Sleep(3000);
+        }
+    }
+}
+
 
 app.UseMiddleware<ApiKeyMiddleware>();
 
